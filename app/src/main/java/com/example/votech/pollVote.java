8679531cef89b299b89edd8e8bdd7e8f9c342ee1
Youssef Wageeh <youssef1604941@miuegypt.edu.eg>
2019-12-08 10:49:24 +0200

    12

--- app/src/main/java/com/example/votech/pollVote.java
+++ app/src/main/java/com/example/votech/pollVote.java
@@ -3,17 +3,193 @@ package com.example.votech;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
+import com.backendless.persistence.DataQueryBuilder;
+
+import java.util.List;
 
 public class pollVote extends AppCompatActivity {
 
     String pollID;
+    int pollId;
+    TextView title, description;
+    Button yesButton, noButton, neutralButton;
+    PollUser vote;
+    Polls p;
+    DataQueryBuilder queryBuilder = DataQueryBuilder.create();
+    BackendlessUser user = Backendless.UserService.CurrentUser();
+    int currentUserId = Integer.parseInt(user.getProperty("id").toString());
+    int userType = Integer.parseInt(user.getProperty("UserTypeID").toString());
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poll_vote);
-
+        vote = new PollUser();
+        p = new Polls();
         pollID=getIntent().getStringExtra("pollID");
-        Toast.makeText(this, pollID, Toast.LENGTH_SHORT).show();
+        pollId = Integer.parseInt(pollID);
+        title = findViewById(R.id.Title);
+        description = findViewById(R.id.Description);
+        yesButton = findViewById(R.id.Yes);
+        noButton = findViewById(R.id.No);
+        neutralButton = findViewById(R.id.Neutral);
+        vote.setPollID(pollId);
+        String whereClause = "pollID = " + pollId + " AND UserID = " + currentUserId;
+        Backendless.Data.of(Polls.class).find(queryBuilder.setWhereClause("id = " + pollId), new AsyncCallback<List<Polls>>() {
+            @Override
+            public void handleResponse(List<Polls> poll) {
+                title.setText(poll.get(0).getTitle());
+                description.setText(poll.get(0).getDescription());
+            }
+
+            @Override
+            public void handleFault(BackendlessFault fault) {
+
+            }
+        });
+        Backendless.Data.of(PollUser.class).find(queryBuilder.setWhereClause(whereClause), new AsyncCallback<List<PollUser>>() {
+            @Override
+            public void handleResponse(List<PollUser> response) {
+                if(response.size() > 0 || userType == 1)
+                {
+                    yesButton.setClickable(false);
+                    noButton.setClickable(false);
+                    neutralButton.setClickable(false);
+                }
+            }
+
+            @Override
+            public void handleFault(BackendlessFault fault) {
+
+            }
+        });
+
+    }
+    public void vote(View view)
+    {
+        int buttonId = view.getId();
+        Toast.makeText(this, "Voted Successfully!", Toast.LENGTH_LONG).show();
+        yesButton.setClickable(false);
+        noButton.setClickable(false);
+        neutralButton.setClickable(false);
+        Backendless.Data.of(PollUser.class).getObjectCount(new AsyncCallback<Integer>() {
+            @Override
+            public void handleResponse(Integer count) {
+                vote.setId(count + 1);
+                vote.setUserID(currentUserId);
+            }
+
+            @Override
+            public void handleFault(BackendlessFault fault) {
+
+            }
+        });
+        Backendless.Persistence.of(PollUser.class).save(vote, new AsyncCallback<PollUser>() {
+            @Override
+            public void handleResponse(PollUser response) {
+
+            }
+
+            @Override
+            public void handleFault(BackendlessFault fault) {
+
+            }
+        });
+        if (buttonId == R.id.Yes)
+        {
+            final AsyncCallback<Polls> updateResponder = new AsyncCallback<Polls>() {
+                @Override
+                public void handleResponse(Polls response) {
+
+                }
+
+                @Override
+                public void handleFault(BackendlessFault fault) {
+
+                }
+            };
+            Backendless.Data.of(Polls.class).find(queryBuilder.setWhereClause("id = " + pollId), new AsyncCallback<List<Polls>>() {
+                @Override
+                public void handleResponse(List<Polls> poll) {
+                    int currentYes = poll.get(0).getYes();
+                    int currentTotal = poll.get(0).getTotalVotes();
+                    p.setYes(currentYes + 1);
+                    p.setTotalVotes(currentTotal + 1);
+                    Backendless.Data.of(Polls.class).save(p, updateResponder);
+                }
+
+                @Override
+                public void handleFault(BackendlessFault fault) {
+
+                }
+            });
+        }
+        else if(buttonId == R.id.No)
+        {
+            final AsyncCallback<Polls> updateResponder = new AsyncCallback<Polls>() {
+                @Override
+                public void handleResponse(Polls response) {
+
+                }
+
+                @Override
+                public void handleFault(BackendlessFault fault) {
+
+                }
+            };
+            Backendless.Data.of(Polls.class).find(queryBuilder.setWhereClause("id = " + pollId), new AsyncCallback<List<Polls>>() {
+                @Override
+                public void handleResponse(List<Polls> poll) {
+                    int currentNo = poll.get(0).getNo();
+                    int currentTotal = poll.get(0).getTotalVotes();
+                    p.setNo(currentNo + 1);
+                    p.setTotalVotes(currentTotal + 1);
+                    Backendless.Data.of(Polls.class).save(p, updateResponder);
+                }
+
+                @Override
+                public void handleFault(BackendlessFault fault) {
+
+                }
+            });
+        }
+        else if(buttonId == R.id.Neutral)
+        {
+            final AsyncCallback<Polls> updateResponder = new AsyncCallback<Polls>() {
+                @Override
+                public void handleResponse(Polls response) {
+
+                }
+
+                @Override
+                public void handleFault(BackendlessFault fault) {
+
+                }
+            };
+            Backendless.Data.of(Polls.class).find(queryBuilder.setWhereClause("id = " + pollId), new AsyncCallback<List<Polls>>() {
+                @Override
+                public void handleResponse(List<Polls> poll) {
+                    int currentNeutral = poll.get(0).getNeutral();
+                    int currentTotal = poll.get(0).getTotalVotes();
+                    p.setNeutral(currentNeutral + 1);
+                    p.setTotalVotes(currentTotal + 1);
+                    Backendless.Data.of(Polls.class).save(p, updateResponder);
+                }
+
+                @Override
+                public void handleFault(BackendlessFault fault) {
+
+                }
+            });
+        }
+
     }
 }
